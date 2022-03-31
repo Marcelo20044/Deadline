820ea0bc9765b9e734002d8daf2a5f662520dd8c
Marcelo <khmark04@mail.ru>
2022-03-31 23:08:40 +0300

    error

--- src/test/java/DbUtils.java
+++ src/test/java/DbUtils.java
@@ -1,11 +1,10 @@
-import com.github.javafaker.Faker;
-import lombok.val;
+
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+
 
 
 public class DbUtils {
@@ -13,13 +12,14 @@ public class DbUtils {
 
 
     public User getUser() {
-        var dataSQL = "SELECT * FROM users;";
+
+        String userSQL = "SELECT * FROM users;";
 
         User user = null;
         try (
                 Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass")
         ) {
-            user = runner.query(conn, dataSQL, new BeanHandler<>(User.class));
+            user = runner.query(conn, userSQL, new BeanHandler<>(User.class));
         } catch (SQLException e) {
             e.printStackTrace();
         }
