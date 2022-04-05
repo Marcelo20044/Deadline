9e63743c63059a8a760d4337940265006c78bab5
Marcelo <khmark04@mail.ru>
2022-04-05 21:28:38 +0300

    id

--- src/test/java/DbUtils.java
+++ src/test/java/DbUtils.java
@@ -1,9 +1,10 @@
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
 
 
 public class DbUtils {
@@ -23,6 +24,40 @@ public class DbUtils {
             e.printStackTrace();
         }
         return user;
+    }
+
+    public String getPasswordByLogin() {
+        String password = "123qwerty";
+
+        return password;
+    }
+
+
+    public String getVerificationCode() {
+        String idSQL = "SELECT id FROM users WHERE login = 'petya';";
+
+        String id = null;
+        try (
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass")
+        ) {
+            id = runner.query(conn, idSQL, new ScalarHandler<>());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        String codeSQL = "SELECT code FROM auth_codes WHERE user_id =" + id + " ORDER BY id DESC;";
+
+
+        String code = null;
+
+        try (
+                Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/app", "app", "pass")
+        ) {
+            code = runner.query(conn, codeSQL, new ScalarHandler<>());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return code;
     }
 
 
--- src/test/java/VerificationTest.java
+++ src/test/java/VerificationTest.java
@@ -1,5 +1,8 @@
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -13,7 +16,12 @@ public class VerificationTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999/");
         $("[data-test-id='login'] input").val(us.getLogin());
-        $("[data-test-id='password'] input").val(us.getPassword());
+        $("[data-test-id='password'] input").val(db.getPasswordByLogin());
+        $("[data-test-id=action-login]").click();
+        $("[data-test-id=code] input").val(db.getVerificationCode());
+        $("[data-test-id='action-verify']").click();
+        $("[id='root']").shouldBe(Condition.visible)
+                 .shouldHave(text("Личный кабинет"));
 
     }
 }
